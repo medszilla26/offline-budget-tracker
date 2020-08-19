f701e42ae7bfc49d991e2202147e5a679d0057f8
M3D5 <pablohdzjr@gmail.com>
2020-08-19 00:13:50 -0500

    added files to cache in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,10 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/styles.css",
-  "/dist/app.bundle.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  //   "/dist/app.bundle.js",
 ];
 
 const PRECACHE = "precache-v1";
