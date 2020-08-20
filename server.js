31a8b82ad20f7efffa1e764ecefba05daef1054e
M3D5 <pablohdzjr@gmail.com>
2020-08-19 22:43:09 -0500

    added mongo atlas connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -15,11 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
-
+let uri = "mongodb://localhost/budget";
+if (process.env.NODE_ENV === "production") {
+  uri = process.env.MONGODB_URI;
+}
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // routes
 app.use(require("./routes/api"));
 
